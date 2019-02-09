0fc3eaa997fdebe9da060e3214689f42881b8151
adelcccp <42842492+adelcccp@users.noreply.github.com>
2019-02-08 18:48:26 -0600

    Add files via upload

--- LoginTests.java
+++ LoginTests.java
@@ -6,26 +6,62 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
-
+//Login tests are maintained here.
+//without Page object approach
 public class LoginTests {
 
     WebDriver driver;
-    @BeforeClass
+
+    @BeforeMethod
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
+
     @Test
     public void loginTest1() {
         driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
         driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
-        Assert.assertEquals(driver.getTitle(), "Web Orders");
+        String title = driver.getTitle();
+        Assert.assertEquals(title, "Web Orders");
     }
+
+    @Test
+    public void negativeloginTest() {
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester2");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test2" + Keys.ENTER);
+        String errorMsg = driver.findElement(By.id("ctl00_MainContent_status")).getText();
+
+        Assert.assertEquals(errorMsg, "Invalid Login or Password.");
+    }
+
+
+    @Test
+    public void logOutTest() {
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
+
+        driver.findElement(By.id("ctl00_logout")).click();
+        String title = driver.getTitle();
+        Assert.assertEquals(title, "Web Orders Login");
+
+    }
+
+
+    @AfterMethod
+    public void cleanUp() {
+        driver.close();
+    }
+
 }
